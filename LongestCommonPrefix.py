bbd25cee1b97bb31b986437da16dc3928f4a4df5
c8763yee <49835073+c8763yee@users.noreply.github.com>
2023-05-10 01:38:43 +0800

    Delete LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        s = ''
-        for i in range(min(map(len, (strs)))):
-            if all(x[i] == strs[0][i] for x in strs) is False:
-                return s
-            
-            s += strs[0][i]
-        return s
