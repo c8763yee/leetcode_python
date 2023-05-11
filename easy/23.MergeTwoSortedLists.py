d377d1372f3e368ece5d76514b0270482588cd2b
c8763yee <rrrrr4413@gmail.com>
2023-05-11 13:11:47 +0800

    status 13/203

---
+++ easy/23.MergeTwoSortedLists.py
@@ -0,0 +1,14 @@
+class Solution
+    def mergeTwoLists(self, list1, list2):
+        def merge(l1, l2):
+            if not l1:
+                return l2
+            if not l2:
+                return l1
+            if l1.val < l2.val:
+                l1.next = merge(l1.next, l2)
+                return l1
+            l2.next = merge(l1, l2.next)
+            return l2
+
+        return merge(list1, list2)
