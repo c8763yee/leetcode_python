85296693345c753a0505991891f5491ccf6b8972
c8763yee <rrrrr4413@gmail.com>
2023-05-15 13:18:03 +0800

    status: 24/205

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "status: $(ls -aRl | grep .py | wc -l)/$1)"
-
-#git push
-#cp .vimrc  ~/.vimrc
+git commit -m "status: $(ls -aRl | grep .py | wc -l)/$1"
+git push
+cp .vimrc  ~/.vimrc
