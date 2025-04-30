49e073f30988dec27b2c68e85e8cc09528a9c5e0
c8763yee <rrrrr4413@gmail.com>
2025-04-30 14:35:18 +0800

    status: 213/null

--- easy/1295.FindNumbersWithEvenNumberOfDIgits/python3/solution.py
+++ easy/1295.FindNumbersWithEvenNumberOfDIgits/python3/solution.py
@@ -1,5 +1,8 @@
 from typing import List
 
+
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        return sum(i % 100 >= 10 or i % 100000 == 0 for i in nums)
+        return sum(
+            i == 0 or 10 <= i < 100 or 1000 <= i < 10000 or i == 100000 for i in nums
+        )
